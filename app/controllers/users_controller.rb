64e2a57c367886c425c2dd6d00bf20ee226becc6
Yuuki <ukmmm0523@gmail.com>
2021-08-05 11:10:25 +0900

    ログイン画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
-      else
-        render :new
+      redirect_to user_path(@user.id)
+    else
+      render :new
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   private
--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Ig
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+    
+    g.assets false
+    g.helper false
+   end
+  
   end
 end
